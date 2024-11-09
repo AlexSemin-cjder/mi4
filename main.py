7ad4d352fedf32c9ae0e18f579bc8ac8596828a8
AlexSemin-cjder <119964665+AlexSemin-cjder@users.noreply.github.com>
2024-11-09 20:43:45 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+name = 'Name: Alex '
+print(name)
+age = '41'
+print( 'Age:' + age)
+New_age = (int( age)) +1
+print ( 'Nev age:' + (str( New_age)) )
+Is_student = 'True'
+print('Is student:' + Is_student)
